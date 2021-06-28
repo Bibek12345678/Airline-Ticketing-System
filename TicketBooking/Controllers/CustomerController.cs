885358e2790647227ebb3729c1df7c5b720f33de
Bibek Neupane <neupanebibek14@gmail.com>
2021-06-28 19:01:25 +0545

    Ajax Submitted

---
+++ TicketBooking/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicketBooking.DAL;
+using TicketBooking.Models;
+
+namespace TicketBooking.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private BookingContext db = new BookingContext();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var list = db.Customers.ToList();
+            return View(list);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            Customer cus = new Customer();
+            return View(cus);
+        }
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Create");
+        }
+    }
+}
---
+++ TicketBooking/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TicketBooking.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerNo { get; set; }
+        public string CustomerName { get; set; }
+        public int Salary { get; set; }
+        public string DepartName { get; set; }
+
+
+    }
+}
